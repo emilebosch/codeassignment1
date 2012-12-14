cf7d45df9e2524179a1d5cd9aea1260d14438341
Samir Boulema <samirboulema@funda.nl>
2012-12-14 17:14:19 +0100

    Added Retry

--- OVImport/Lib/OVTransactionImport.cs
+++ OVImport/Lib/OVTransactionImport.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using MbUnit.Framework;
 using Microsoft.VisualBasic.FileIO;
 using System.Configuration;
 using log4net;
@@ -23,39 +25,61 @@ namespace OVImport
             api = new SimpleRestApi(ConfigurationManager.AppSettings["ovservice"]);
             log4net.Config.XmlConfigurator.Configure();
 
-            parser = new TextFieldParser(csvfile);
-            parser.Delimiters = new[] { "," };
+            parser = new TextFieldParser(csvfile) {Delimiters = new[] {","}};
             parser.ReadFields();
 
             while (!parser.EndOfData)
-            {
-                var csvFields = parser.ReadFields();
-                var apiresponse =
-                    api.Post("ovtransactionimport/process",
-                    new
-                    {
-                        id = csvFields[0],
-                        date = csvFields[1],
-                        station = csvFields[2],
-                        action = csvFields[3],
-                        cardid = Convert.ToInt64(csvFields[4]),
-                        userid = Convert.ToInt64(csvFields[5])
-                    },
-                    new
-                    {
-                        success = default(Boolean),
-                        error = default(String)
-                    });
+            {                
+                IList<string> csvFields = parser.ReadFields();
+                Line CSVLine = new Line(csvFields);
 
-                if (apiresponse.success)
-                {
-                    log.Info("Succesfully uploaded transaction!");
-                }
-                else
-                {
-                    log.ErrorFormat("API responsed with an error {0}", apiresponse.error);
-                }
+                Retry.Repeat(3)
+                     .WithPolling(TimeSpan.FromSeconds(1))
+                     .WithTimeout(TimeSpan.FromSeconds(10))
+                     .Until(() => PostCSVLine(CSVLine));
             }
+        }
+
+        private bool PostCSVLine(Line line)
+        {
+            APIResponse response = api.Post("ovtransactionimport/process", line, new APIResponse());
+
+            if (response.success)
+            {
+                log.Info("Succesfully uploaded transaction!");
+            }
+            else
+            {
+                log.ErrorFormat("API responsed with an error {0}", response.error);
+            }
+
+            return response.success;
+        }
+
+        public class Line
+        {
+            public Line(IList<string> csvFields)
+            {
+                id = Convert.ToInt64(csvFields[0]);
+                date = csvFields[1];
+                station = csvFields[2];
+                action = csvFields[3];
+                cardid = Convert.ToInt64(csvFields[4]);
+                userid = Convert.ToInt64(csvFields[5]);
+            }
+
+            public long id;
+            public string date;
+            public string station;
+            public string action;
+            public long cardid;
+            public long userid;
+        }
+
+        public class APIResponse
+        {
+            public bool success;
+            public string error;
         }
     }
 }
